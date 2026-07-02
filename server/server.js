b25e61416c95f4a7a342a018521120e42c48ef8f
TanyaVaish-17 <tanyavaish05@gmail.com>
2026-07-02 23:24:02 +0530

    updating deployment changes

--- server/server.js
+++ server/server.js
@@ -9,7 +9,12 @@ connectDB();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://taskverse-yourname.vercel.app', 
+  ],
+}));
 app.use(express.json());
 
 app.get('/', (req, res) => {
